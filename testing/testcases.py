64b61f63ecd14078cc2c263d002cf7ce52c21e2b
cc <calliechen@Callies-MacBook.lan>
2021-12-23 11:31:21 -0500

    not sure what i added

--- testing/testcases.py
+++ testing/testcases.py
@@ -1,8 +1,19 @@
 from django.test import TestCase as DjangoTestCase
 from django.contrib.auth.models import User
 from tweets.models import Tweet
+from rest_framework.test import APIClient
+
 
 class TestCase(DjangoTestCase):
+
+    @property
+    def anonymous_client(self):
+        #instance level 的cache
+        #如果同一个self进来， 不会生成新的client
+        if hasattr(self, '_anonymous_client'):
+            return self._anonymous_client
+        self._anonymous_client = APIClient()
+        return self._anonymous_client
 
     def create_user(self, username, email, password = None):
         if password is None:
