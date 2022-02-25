394eea183368a44c3321c66614fedf2f97272468
cc <calliechen@Callies-MacBook.lan>
2022-02-25 11:21:08 -0500

    added cache for newsfeeds

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -1,10 +1,10 @@
+from newsfeeds.api.serializers import NewsFeedSerializer
+from newsfeeds.models import NewsFeed
+from newsfeeds.services import NewsFeedService
 from rest_framework import viewsets, status
 from rest_framework.decorators import permission_classes
-
-from rest_framework.permissions import  IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from newsfeeds.models import NewsFeed
-from newsfeeds.api.serializers import NewsFeedSerializer
 from utils.paginations import EndlessPagination
 
 
@@ -12,16 +12,12 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
     permission_classes = (IsAuthenticated,)
     pagination_class = EndlessPagination
 
-    def get_queryset(self):
-        return NewsFeed.objects.filter(user = self.request.user)
-
     def list(self, request):
-        newsfeed = self.get_queryset()
+        newsfeed = NewsFeedService.get_cached_newsfeeds(user_id=self.request.user.id)
         page = self.paginate_queryset(newsfeed)
         serializer = NewsFeedSerializer(
             page,
-            context={'request':request},
-            many = True,
+            context={'request': request},
+            many=True,
         )
         return self.get_paginated_response(serializer.data)
-
--- newsfeeds/services.py
+++ newsfeeds/services.py
@@ -1,21 +1,39 @@
 from friendships.services import FriendshipService
 from newsfeeds.models import NewsFeed
-
+from twitter.cache import USER_NEWSFEEDS_PATTERN
+from utils.redis_helper import RedisHelper
 #service一般都是class method, 不需要new instance
+
+
 class NewsFeedService(object):
 
     @classmethod
     def fanout_to_followers(cls, tweet):
         followers = FriendshipService.get_followers(tweet.user)
-        #production 里不允许for + sql query
-       # for follower in followers:
-            #NewsFeed.objects.create(user = follower, tweet = tweet)
+        # production 里不允许for + sql query
+        # for follower in followers:
+        # NewsFeed.objects.create(user = follower, tweet = tweet)
         newsfeeds = [
             NewsFeed(user = follower, tweet = tweet)
             for follower in followers
         ]
         newsfeeds.append(NewsFeed(user = tweet.user, tweet = tweet))
         NewsFeed.objects.bulk_create(newsfeeds)
+        # bulk create 不会触发 post_save 的 signal，所以需要手动 push 到 cache 里
+        for newsfeed in newsfeeds:
+            cls.push_newsfeed_to_cache(newsfeed)
+
+    @classmethod
+    def get_cached_newsfeeds(cls, user_id):
+        queryset = NewsFeed.objects.filter(user_id=user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=user_id)
+        return RedisHelper.load_objects(key, queryset)
+
+    @classmethod
+    def push_newsfeed_to_cache(cls, newsfeed):
+        queryset = NewsFeed.objects.filter(user_id=newsfeed.user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=newsfeed.user_id)
+        RedisHelper.push_object(key, newsfeed, queryset)
 
 
 
